b664143eb83ba0fcc7e7dcb8785949e9d8ac4411
dmytroafr <afrosin.dmytro@gmail.com>
2023-05-20 10:21:07 +0300

    studying junit + @test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello MF's");
-
+        Calculator calc = new Calculator();
+        CalculatorTest calcTest = new CalculatorTest();
+        calcTest.evaluatesExpression();
         
     }
 }
